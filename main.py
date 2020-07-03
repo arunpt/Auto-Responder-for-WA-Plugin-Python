3bb78c9204b1e12af152300fdb98b4b92f435e2e
justinthoms <57831354+justinthoms@users.noreply.github.com>
2020-07-03 17:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def command_handler():
         return send_message(f"Im alive 😏\n_Powered by : auto responder plugin by mr JUSTIN")
     if cmd == "/help":
         return send_message(plugin.help())
-    if cmd == "/covid":(f"*this feeture coming soon!*")
-        return send_message(plugin.covid())
+    if cmd == "/covid":
+        return send_message(f"*this feeture coming soon!*")
     elif cmd == "/tr":
         return send_message(plugin.translate(msg))
     elif cmd == "/wiki":
