4387894e91a2050d8af9542c72df5c80c95aedde
justinthoms <57831354+justinthoms@users.noreply.github.com>
2020-07-08 09:37:16 +0530

    Update addons.py

--- addons.py
+++ addons.py
@@ -1,8 +1,10 @@
 import wikipedia
 import urbandict
+import sys
 import requests
 from googletrans import Translator
 from bs4 import BeautifulSoup
+
 
 class Plugin:   
     def help(self):
@@ -12,7 +14,24 @@ class Plugin:
                "/dict <word>\n" \
                "/maldict <word>\n" \
                "/wiki <search query>\n" \
+               "/mod <app name>\n" \
                
+    def mod(self, app_name):
+        url = f"https://moddroid.com/?s={app_name.replace(' ','+')}"
+        page = BeautifulSoup(requests.get(url).content,"html.parser")
+        content = page.find('div',{'class':'rd-details-top'})
+        if content:
+            app_url = content.find('a', href=True)['href']
+            dwn_page = BeautifulSoup(requests.get(app_url + "?download").content,"html.parser")
+            app_urls = dwn_page.find('div',{'id':'download-tab'})
+            links = ["*Available versions are:*\n\n"]
+            [links.append(dwn_url.get_text() + "\n" + dwn_url['href'] + "\n\n") \
+               for dwn_url in app_urls.find_all('a', href=True)]
+            dwn_url = ''.join(links)
+            return dwn_url
+        else:
+            return "no results"
+           
     def translate(self, message):
         try:
             lan_code,text = message.split("=")
@@ -26,6 +45,17 @@ class Plugin:
             return f"*Translated from {translated.src} to {lan_code}* \n_{translated.text}_"
         except Exception as err:
             return str(err) 
+        
+   def mod(self, app_name):
+url = requests.get('https://www.flipkart.com/search?q={app_name.replace(' ','+')}')
+content = r.content.decode(encoding='UTF-8')
+soup = BeautifulSoup(r.content.decode(encoding='UTF-8'), "lxml")
+reviews = soup.find_all('div', {"class": "_3ULzGw"})
+for item in reviews:
+      return item
+        else:
+            return "no results"
+        
                 
     def olam(self, query):
         if not query:
